6713a58b306cd36bb529a2a4e64d040619ebbef2
IrinaAlcala <58370677+IrinaAlcala@users.noreply.github.com>
2021-02-21 16:54:42 -0800

    added state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import Header from './components/Header'
 import './App.css';
 import Tasks from './components/Tasks';
+import { useState } from 'react';
 
-function App() {
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+  {
+  id: 1,
+  text: "Doctors appointment",
+  day: "feb 5 at 2;30",
+  reminder: true,
+},
+{
+  id: 2,
+  text: "Meeting at School",
+  day: "feb 6 at 1:30",
+  reminder: true,
+},
+  ])
+
+  
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,23 +1,9 @@
 import React from 'react';
 
 
-const tasks = [
-    {
-      id: 1,
-      text: "Doctors Appointment",
-      day: "Feb 5th at 2:30pm",
-      reminder: true
-    },
-    {
-      id: 2,
-      text: "Meeting at School",
-      day: "Feb 6th at 1:30pm",
-      reminder: true
-    }
-    
-]
 
-const Tasks = () => {
+const Tasks = ({ tasks }) => {
+   
     return (
         <>
         {tasks.map((task) => (
