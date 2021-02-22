c3a98563328dbd846bf5c72664ee337d56357558
IrinaAlcala <58370677+IrinaAlcala@users.noreply.github.com>
2021-02-21 16:33:11 -0800

    added tasks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from './components/Header'
 import './App.css';
+import Tasks from './components/Tasks';
 
 function App() {
   return (
     <div className="container">
       <Header />
-      
+      <Tasks />
     </div>
   );
 }
