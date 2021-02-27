aa0f246e989cb0927bc4bddb839676eda2ed15f5
IrinaAlcala <58370677+IrinaAlcala@users.noreply.github.com>
2021-02-26 19:01:58 -0800

    added footer and about

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
-// import Footer from './components/Footer'
+import Footer from './components/Footer'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
-// import About from './components/About'
+import About from './components/About'
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false)
@@ -112,8 +112,8 @@ const App = () => {
             </>
           )}
         />
-        {/* <Route path='/about' component={About} />
-        <Footer /> */}
+         <Route path='/about' component={About} />
+        <Footer /> 
       </div>
     </Router>
   )
